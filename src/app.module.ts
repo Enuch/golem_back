fb56a2b6c88592aba836da5777135ae5b668cb7a
Enuch <enuchsantys@gmail.com>
2022-11-02 19:34:17 -0300

    Corrigindo conflitos com modulos de material_request e erro do PrismaService faltando no provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,16 @@ import { UserModule } from './modules/user/user.module';
 import { CategoryModule } from './modules/category/category.module';
 import { MaterialModule } from './modules/material/material.module';
 import { RequestModule } from './modules/request/request.module';
-import { MaterialRequestModule } from './modules/material.request/material.request.module';
 import { MaterialRequestModule } from './modules/material_request/material_request.module';
 
 @Module({
-  imports: [UserModule, CategoryModule, MaterialModule, RequestModule, MaterialRequestModule],
+  imports: [
+    UserModule,
+    CategoryModule,
+    MaterialModule,
+    RequestModule,
+    MaterialRequestModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/modules/material_request/material_request.module.ts
+++ src/modules/material_request/material_request.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MaterialRequestService } from './material_request.service';
 import { MaterialRequestController } from './material_request.controller';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Module({
   controllers: [MaterialRequestController],
-  providers: [MaterialRequestService]
+  providers: [MaterialRequestService, PrismaService],
 })
 export class MaterialRequestModule {}
