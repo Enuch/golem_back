ca84df5272730885eb45ef227a9fa3c9fba04441
Enuch <enuchsantys@gmail.com>
2022-11-07 03:10:51 -0300

    Alterando chaves de retorno do objeto na autenticação

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -21,7 +21,7 @@ export class AuthService {
   }
 
   async login(user: UserDTO) {
-    const payload = { username: user.username, sub: user.id, role: user.role };
+    const payload = { username: user.username, id: user.id, role: user.role };
     return {
       access_token: this.jwtService.sign(payload),
       user: payload,
